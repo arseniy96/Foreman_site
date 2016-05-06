cb500c8493392aa46125d83feba7c7ce134a167e
arseniy96 <arsenzhar@gmail.com>
2016-05-06 19:46:17 +0300

    Commit by idib

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,24 +4,12 @@ $(document).ready(function(){
     var str = 'class="active"';
     var strr = 'active';
     eps = $(window).height()*0.1;
-    $(".item.slid").each(function(i){
-        $(this).addClass(strr);
-        $("ol.carousel-indicators").append(
-            '<li data-target="#carousel-example-generic" data-slide-to="'+ i.toString() +'"'+str.toString()+'></li>'
-        );
-        str = strr = "";
-    });
-    window.onscroll = function(){
-        var pos = $('#NavBar').offset().top - $(window).scrollTop();
-        var posabs = $('#Abspos').offset().top - $(window).scrollTop();
-        console.log(pos);
-        console.log(eps);
-        if(pos <= eps)
-        {
-            $("#NavBar").addClass('navbar-fixed-top');
-        }
-        if(posabs > eps){
-            $("#NavBar").removeClass('navbar-fixed-top');
-        }
-    }
+    if($(".item.slid").length !=  $("ol>li").length)
+        $(".item.slid").each(function(i){
+            $(this).addClass(strr);
+            $("ol.carousel-indicators").append(
+                '<li data-target="#carousel-example-generic" data-slide-to="'+ i.toString() +'"'+str.toString()+'></li>'
+            );
+            str = strr = "";
+        });
 });
