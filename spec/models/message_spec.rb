bc543060af3bb5dd8e788e0b5fce668b974a1dc9
arseniy96 <arsenzhar@gmail.com>
2016-05-07 17:06:32 +0300

    Update spec files

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 describe Message do
   it { should validate_presence_of :text }
   it { should validate_presence_of :username }
+  it { should validate_length_of(:text).is_at_most(400) }
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :username }
+  it { should validate_presence_of :phone }
+  it { should validate_length_of(:message).is_at_most(400) }
 end
