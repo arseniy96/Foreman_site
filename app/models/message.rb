31e715e39e686c2c8f015e781914ac7406e4eeb9
arseniy96 <arsenzhar@gmail.com>
2016-05-07 17:05:47 +0300

    Add validations

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,4 @@
 class Message < ActiveRecord::Base
-  validates :text, presence: true
+  validates :text, presence: true, length: {maximum: 400}
   validates :username, presence: true
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,5 @@
 class Order < ActiveRecord::Base
+  validates :username, presence: true
+  validates :phone, presence: true
+  validates :message, length: {maximum: 400}
 end
