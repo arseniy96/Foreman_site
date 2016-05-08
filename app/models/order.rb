81570c189f2f25730067f327e724a72b55863ebc
arseniy96 <arsenzhar@gmail.com>
2016-05-08 23:33:18 +0300

    Add validation for length

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ActiveRecord::Base
-  validates :username, presence: true
+  validates :username, presence: true, length: {minimum: 2}
   validates :phone, presence: true
   validates :message, length: {maximum: 400}
 end
